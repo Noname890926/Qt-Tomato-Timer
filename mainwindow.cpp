c316506bc71023b6d707da4e71651131c4388d00
Noname890926 <vincentff7@live.cn>
2019-04-07 22:13:23 +0800

    添加开始计时功能

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,47 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QTime>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    _isStart = false;     //初始为还未计时
+    _timer = new QTimer;  //初始化定时器
+    _TimeRecord = new QTime(0, 0, 0); //初始化时间
+
+    //QTime time = QTime::currentTime();
+    ui->WorkingTimer->setDigitCount(8);
+    ui->WorkingTimer->setSegmentStyle(QLCDNumber::Flat);
+    ui->WorkingTimer->display(_TimeRecord->toString("hh:mm:ss"));
+
+    connect(_timer,SIGNAL(timeout()),this,SLOT(updateTime()));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::updateTime()
+{
+    *_TimeRecord = _TimeRecord->addSecs(1);
+    ui->WorkingTimer->display(_TimeRecord->toString("hh:mm:ss"));
+}
+
+void MainWindow::on_WorkingTimer_overflow()
+{
+
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    _timer->start(1000);
+    //while(true)
+    //{
+    //    ui->WorkingTimer->display(time.toString("hh:mm:ss"));
+    //}
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QTimer>
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +16,19 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+
+    void updateTime();
+
+    void on_WorkingTimer_overflow();
+
+    void on_pushButton_clicked();
+
 private:
     Ui::MainWindow *ui;
+    QTimer * _timer;      //定时器 每秒更新时间
+    QTime * _TimeRecord;  //记录时间
+    bool _isStart;        //记录是否已经开始计时
 };
 
 #endif // MAINWINDOW_H
